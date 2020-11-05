6ad21d4069e3ced4c8c0154977fc42479d00b899
Guillermo de Pablos <guillermo.depablos@innocv.com>
2020-11-05 22:50:20 +0100

    Update Api Gateway

---
+++ TiendaServicios.Api.Gateway/ImplementInterface/AutorRemote.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Gateway.InterfaceRemote;
+using TiendaServicios.Api.Gateway.LibroRemote;
+
+namespace TiendaServicios.Api.Gateway.ImplementInterface
+{
+    public class AutorRemote : IAutorRemote
+    {
+        private readonly ILogger<AutorRemote> _logger;
+        private readonly IHttpClientFactory _httpClient;
+        public AutorRemote(ILogger<AutorRemote> logger, IHttpClientFactory httpClient)
+        {
+            _logger = logger;
+            _httpClient = httpClient;
+        }
+        public async Task<(bool resultado, AutorModeloRemote autor, string Error)> GetAutor(Guid autorId)
+        {
+            try
+            {
+                var client = _httpClient.CreateClient("AutorService");
+                var response = await client.GetAsync($"/Autor/{autorId}");
+                if(response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                    var resultado = JsonSerializer.Deserialize<AutorModeloRemote>(content, options);
+                    return (true, resultado, null);
+                }
+                return (false, null, response.ReasonPhrase);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
---
+++ TiendaServicios.Api.Gateway/MessageHandler/LibroHandler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Gateway.InterfaceRemote;
+using TiendaServicios.Api.Gateway.LibroRemote;
+
+namespace TiendaServicios.Api.Gateway.MessageHandler
+{
+    public class LibroHandler : DelegatingHandler
+    {
+        private readonly ILogger<LibroHandler> _logger;
+        private readonly IAutorRemote _autorRemote;
+        public LibroHandler(ILogger<LibroHandler> logger, IAutorRemote autorRemote)
+        {
+            _logger = logger;
+            _autorRemote = autorRemote;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellation)
+        {
+            var time = Stopwatch.StartNew();
+            _logger.LogInformation("Inicio request");
+            var response = await base.SendAsync(request, cancellation);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                var resultado = JsonSerializer.Deserialize<LibroModeloRemote>(content, options);
+                var responseAutor = await _autorRemote.GetAutor(resultado.AutorLibro ?? Guid.Empty);
+                if(responseAutor.resultado)
+                {
+                    var objetoAutor = responseAutor.autor;
+                    resultado.AutorData = objetoAutor;
+                    var resultadoString = JsonSerializer.Serialize(resultado);
+                    response.Content = new StringContent(resultadoString, System.Text.Encoding.UTF8, "application/json");
+                }
+            }
+
+            _logger.LogInformation($"Proceso se hizo en {time.ElapsedMilliseconds}ms");
+            return response;
+        }
+
+    }
+}
